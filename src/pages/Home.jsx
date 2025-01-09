2811929da0765fc9233d7f04273d2a6ff3b35f26
cueva91 <fondocapitum2024@gmail.com>
2025-01-09 10:43:00 -0500

    ajuste de cv_Divar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
-import githubIcon from '../assets/icon-github.png';  // Asegúrate de que la ruta sea correcta
-import linkedinIcon from '../assets/icon-linkedin.png';
-import resumePDF from '../assets/CV_Divar.pdf'; // Asegúrate de que la ruta sea correcta
+import githubIcon from "../assets/icon-github.png"; // Asegúrate de que la ruta sea correcta
+import linkedinIcon from "../assets/icon-linkedin.png";
+import resumePDF from "../assets/cv_Divar.pdf"; // Asegúrate de que la ruta sea correcta
 
 const Home = () => {
   const { t, i18n } = useTranslation(); // Hook de traducción
 
-    // Función para hacer scroll suave
-    const scrollToSection = (sectionId) => {
-      const section = document.getElementById(sectionId);
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-      }
-    };
+  // Función para hacer scroll suave
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-    // Función para cambiar el idioma
-    const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
-    };
+  // Función para cambiar el idioma
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <div>
@@ -32,8 +32,12 @@ const Home = () => {
 
       {/* Botón para cambiar de idioma */}
       <div className="flex justify-end p-4">
-        <button onClick={() => changeLanguage('es')} className="mx-2">ES</button>
-        <button onClick={() => changeLanguage('en')} className="mx-2">EN</button>
+        <button onClick={() => changeLanguage("es")} className="mx-2">
+          ES
+        </button>
+        <button onClick={() => changeLanguage("en")} className="mx-2">
+          EN
+        </button>
       </div>
 
       {/* Sección de perfil */}
@@ -53,16 +57,16 @@ const Home = () => {
         {/* Texto de presentación */}
         <div className="text-center md:text-left max-w-lg">
           <h1 className="text-6xl font-bold mb-6 leading-tight text-white">
-            {t('greeting', { name: '' })}
+            {t("greeting", { name: "" })}
             <span className="text-teal-400">Divar</span>
           </h1>
 
           <h2 className="text-2xl font-semibold text-gray-300 mb-4">
-            {t('developer')}
+            {t("developer")}
           </h2>
 
           <p className="max-w-xl mt-4 text-gray-300 leading-relaxed">
-            {t('description')}
+            {t("description")}
           </p>
 
           {/* Iconos de redes sociales con efecto hover */}
@@ -73,7 +77,11 @@ const Home = () => {
               rel="noopener noreferrer"
               className="hover:opacity-80 transition-opacity transform hover:scale-110"
             >
-              <img src={githubIcon} alt="GitHub" className="w-10 h-10 grayscale hover:grayscale-0" />
+              <img
+                src={githubIcon}
+                alt="GitHub"
+                className="w-10 h-10 grayscale hover:grayscale-0"
+              />
             </a>
             <a
               href="https://www.linkedin.com/in/divar-parra-mu%C3%B1oz-338091152/"
@@ -81,7 +89,11 @@ const Home = () => {
               rel="noopener noreferrer"
               className="hover:opacity-80 transition-opacity transform hover:scale-110"
             >
-              <img src={linkedinIcon} alt="LinkedIn" className="w-10 h-10 grayscale hover:grayscale-0" />
+              <img
+                src={linkedinIcon}
+                alt="LinkedIn"
+                className="w-10 h-10 grayscale hover:grayscale-0"
+              />
             </a>
           </div>
 
@@ -94,14 +106,14 @@ const Home = () => {
               rel="noopener noreferrer"
               className="bg-teal-500 hover:bg-teal-600 text-white py-3 px-12 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105"
             >
-              {t('resume')}
+              {t("resume")}
             </a>
 
             <button
               className="bg-gray-700 hover:bg-gray-800 text-white py-3 px-12 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105"
-              onClick={() => scrollToSection('contact')}
+              onClick={() => scrollToSection("contact")}
             >
-              {t('contact')}
+              {t("contact")}
             </button>
           </div>
         </div>
